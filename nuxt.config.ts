367b76c7bd95363871a359bd11c9086af0735e0d
Andrien Wiandyano <arnnied03@gmail.com>
2023-01-17 23:52:01 +0700

    feat: add title, icons, and various meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,72 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "CompeteX",
       bodyAttrs: {
         class: "bg-light",
       },
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/icon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/icon/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/icon/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/icon/site.webmanifest",
+        },
+      ],
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Find Your Next Challenge: Explore Online Competitions From Popular Platforms",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "CompeteX",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "Find Your Next Challenge: Explore Online Competitions From Popular Platforms",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "/icon/android-chrome-512x512.png",
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://competex.vercel.app/",
+        },
+        {
+          hid: "og:site_name",
+          property: "og:site_name",
+          content: "CompeteX",
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "website",
+        },
+      ],
     },
   },
 })
